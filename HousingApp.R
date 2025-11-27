4c9c5775055a07a554cb8c6cd254eb67cd9f274b
Mason Young <masonyou@umich.edu>
2025-11-26 22:29:29 -0500

    added ranges to inputs and regions to graph

--- HousingApp.R
+++ HousingApp.R
@@ -3,7 +3,10 @@ library(ggplot2)
 library(sf)
 library(prettymapr)
 library(ggspatial)
+library(tidyverse)
 theme_set(theme_classic())
+load("a2housing.RData")
+
 
 a2housing_no_missing <- a2housing |> filter(!is.na(lat), !is.na(long))
 a2housing_sf <- st_as_sf(a2housing_no_missing, coords = c("long", "lat"), crs = 4326)
@@ -19,21 +22,21 @@ ui <- fluidPage(
                    label = "Bedrooms: ",
                    min = 0,
                    max = 24,
-                   value = 0
+                   value = c(0, 24)
                  ),
                  sliderInput(
                    inputId = "full_baths", 
                    label = "Full Baths: ",
                    min = 0,
                    max = 24,
-                   value = 0
+                   value = c(0, 24)
                  ),
                  sliderInput(
                    inputId = "half_baths", 
                    label = "Half Baths: ",
                    min = 0,
                    max = 6,
-                   value = 0
+                   value = c(0, 6)
                  ),
                  sliderInput(
                    inputId = "sqft", 
@@ -47,7 +50,7 @@ ui <- fluidPage(
                    label = "Acres: ",
                    min = 0,
                    max = 10,
-                   value = 0
+                   value = c(0, 10)
                  ),
                  selectInput(
                    inputId = "region",
@@ -85,8 +88,8 @@ server <- function(input, output) {
     coefs <- coef(mdl)
     
     predicted_price <- function(num) {
-      signif(coefs[1] + coefs[2] * input$beds + coefs[3] * input$full_baths + 
-      coefs[4] * input$half_baths + coefs[5] * input$sqft[num] + coefs[6] * input$acres, 4)
+      signif(coefs[1] + coefs[2] * input$beds[num] + coefs[3] * input$full_baths[num] + 
+      coefs[4] * input$half_baths[num] + coefs[5] * input$sqft[num] + coefs[6] * input$acres[num], 4)
     }
     
     HTML(paste("Lowest predicted price: $", predicted_price(1), 
@@ -99,7 +102,15 @@ server <- function(input, output) {
     ggplot(data = a2housing_sf) + annotation_map_tile(zoom = 14) + geom_sf() +
       geom_hline(yintercept = y_half, linetype="dashed") + 
       geom_vline(xintercept = x_half, linetype="dashed") +
-      annotate("text", x = -83, y = 42.32, label = "Region 1")
+      annotate("text", x = -83.78, y = 42.33, label = "Region 1", color = "blue") +
+      annotate("text", x = -83.7, y = 42.33, label = "Region 2", color = "blue") +
+      annotate("text", x = -83.78, y = 42.23, label = "Region 3", color = "blue") +
+      annotate("text", x = -83.7, y = 42.23, label = "Region 4", color = "blue") +
+      labs(
+        x = "Longitude", 
+        y = "Latitude", 
+        title = "Map of Ann Arbor with Homes Sold From 2021-2025"
+      )
   })
   
   output$inflation_test_text <- renderText({
